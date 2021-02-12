b6e349c1732f16b486defe7de77ef59716e7115c
radinParsaei <radinparsaei@gmail.com>
2021-02-12 11:57:23 +0330

    fixes in filepath

---
+++ sync_html_files.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+cp test/index.html src/index.html
+sed -i -e 's/<script src="build\/test_bundle.js"><\/script>/<script src="dist\/index.js"><\/script>/g' src/index.html
+rm -f src/index.html-e
