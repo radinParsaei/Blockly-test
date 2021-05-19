bd712db2c53d5345a2fbfb01983a096973221868
radinParsaei <radinparsaei@gmail.com>
2021-05-19 23:45:08 +0430

    now, errors will appear in the console

--- Compiler-web/src/main/java/Targets.java
+++ Compiler-web/src/main/java/Targets.java
@@ -3,6 +3,8 @@ import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLElement;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Targets {
 //    private static final ArrayList<Integer> intervalCodes = new ArrayList<>();
@@ -20,6 +22,39 @@ public class Targets {
         HTMLDocument document = HTMLDocument.current();
         HTMLElement htmlParagraphElement = document.createElement("span");
         htmlParagraphElement.setInnerHTML(value.toString().replace("\n", "<br>").replace(" ", "&nbsp;"));
+        document.getElementById("console2").appendChild(htmlParagraphElement);
+    }
+
+    public static void error(String error) {
+        HTMLDocument document = HTMLDocument.current();
+        HTMLElement htmlParagraphElement = document.createElement("div");
+        htmlParagraphElement.setInnerHTML("<img src=\"test/error.png\" height=\"20\"/><span style=\"position: absolute\">&nbsp;" + error.replace("\n", "<br>").replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") + "</span>");
+        htmlParagraphElement.setAttribute("style", "color: #f55; position: relative");
+        document.getElementById("console2").appendChild(htmlParagraphElement);
+    }
+
+    public static void tokenizerError(int errorChar, String line) { // just for Web(teaVM)
+        StringBuilder error = new StringBuilder("error at line: ");
+        int lineStart = line.lastIndexOf("\n", errorChar) + 1;
+        line = line.substring(0, line.indexOf("\n", errorChar));
+        Pattern pattern = Pattern.compile("\n");
+        Matcher matcher = pattern.matcher(line);
+        int lineNumber = 0;
+        while (matcher.find()) {
+            lineNumber++;
+        }
+        line = line.substring(lineStart);
+        error.append(lineNumber).append("<br/>").append(line).append("<br/>");
+        errorChar -= lineStart;
+        for(; errorChar > 0; errorChar--) {
+            error.append("&nbsp;");
+        }
+        error.append("^");
+
+        HTMLDocument document = HTMLDocument.current();
+        HTMLElement htmlParagraphElement = document.createElement("div");
+        htmlParagraphElement.setInnerHTML("<img src=\"test/error.png\" height=\"20\" style=\"padding-right: 10px\"/><span style=\"position: absolute\">" + error + "</span>");
+        htmlParagraphElement.setAttribute("style", "color: #f55; position: relative");
         document.getElementById("console2").appendChild(htmlParagraphElement);
     }
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 `test/field_minus.js`, `test/field_plus.js`, `test/procedures.js` files copied from `https://github.com/google/blockly-samples/tree/master/plugins/block-plus-minus` and modified
 
 `test/blocks/` files copied from blockly source code (https://github.com/google/blockly) and modified
+
+
+error icon in the web comes from https://www.flaticon.com/premium-icon/cancel_1008927?term=cancel&page=1&position=12&page=1&position=12&related_id=1008927&origin=tag
